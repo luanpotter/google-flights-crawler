49d6edf21e5fb5a88df29f794cae7ae5c064c750
Luan Nico <luanpotter27@gmail.com>
2018-06-09 01:44:23 -0300

    fix

--- storage.js
+++ storage.js
@@ -3,14 +3,13 @@ const { promisify } = require('util');
 const storage = require('@google-cloud/storage');
 const gcs = storage({ projectId: 'lights-io' });
 const bucket = gcs.bucket('crawler-flight-data');
-const upload = promisify(bucket.upload.bind(bucket));
 
 const fs = require('fs');
 const writeFile = promisify(fs.writeFile);
 
 const save = async (path, data) => {
     await writeFile(path, JSON.stringify(data));
-    await upload(path);
+    await new Promise(r => bucket.upload(path, r));
 };
 
 module.exports = { save };
